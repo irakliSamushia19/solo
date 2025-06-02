eb6389b2fb5f1d85ad468eedecf166acdad41dcf
irakliSamushia19 <irakli.samushia.19@gmail.com>
2025-06-02 19:18:01 +0400

    render for netlify

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import {
 import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { CommonEngine } from '@angular/ssr/node';
+import { render } from '@netlify/angular-runtime/common-engine.mjs';
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
