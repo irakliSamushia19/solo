60ac094d33f450d4309394ad68baa3bebb625c91
irakliSamushia19 <irakli.samushia.19@gmail.com>
2025-06-02 19:04:01 +0400

    add some styleing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { NavComponent } from './nav/nav.component';
 import { LifestyleComponent } from './lifestyle/lifestyle.component';
 import { ConverterComponent } from './converter/converter.component';
 import { CurrencyConverterComponent } from './currency-converter/currency-converter.component';
+import { FooterComponent } from './footer/footer.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavComponent, LifestyleComponent, ConverterComponent, CurrencyConverterComponent], 
+  imports: [NavComponent, LifestyleComponent, ConverterComponent, CurrencyConverterComponent, FooterComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
